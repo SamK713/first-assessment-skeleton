2925194c8de3c5d477a970867f03265c59376fb4
Samuel King <samuelmarquiceking@gmail.com>
2017-07-13 06:43:20 -0500

    --message="FA commit 1"--

--- cli/src/cli.js
+++ cli/src/cli.js
@@ -2,8 +2,10 @@ import vorpal from 'vorpal'
 import { words } from 'lodash'
 import { connect } from 'net'
 import { Message } from './Message'
+const colors = require('colors')
 
 export const cli = vorpal()
+
 
 let username
 let server
@@ -12,17 +14,20 @@ cli
   .delimiter(cli.chalk['yellow']('ftd~$'))
 
 cli
-  .mode('connect <username>')
+  .mode('connect <username> ')
   .delimiter(cli.chalk['green']('connected>'))
   .init(function (args, callback) {
     username = args.username
-    server = connect({ host: 'localhost', port: 8080 }, () => {
-      server.write(new Message({ username, command: 'connect' }).toJSON() + '\n')
+    let host = args.host | 'localhost'
+    let port = args.port | 8080
+    server = connect({ host: host, port: port }, () => {
+      server.write(new Message({ username, command: 'connect', contents: ' has connected' }).toJSON() + '\n')
       callback()
     })
 
     server.on('data', (buffer) => {
-      this.log(Message.fromJSON(buffer).toString())
+      let mes = Message.fromJSON(buffer)
+      this.log(mes.toString())
     })
 
     server.on('end', () => {
@@ -34,8 +39,12 @@ cli
     const contents = rest.join(' ')
 
     if (command === 'disconnect') {
-      server.end(new Message({ username, command }).toJSON() + '\n')
-    } else if (command === 'echo') {
+      server.end(new Message({ username, command, contents: ' has disconnected' }).toJSON() + '\n')
+    } else if (command === 'echo' || command === 'broadcast') {
+      server.write(new Message({ username, command, contents }).toJSON() + '\n')
+    } else if (command === 'wisper') {
+      server.write(new Message({ username, command, contents }).toJSON() + '\n')
+    } else if (command === 'users') {
       server.write(new Message({ username, command, contents }).toJSON() + '\n')
     } else {
       this.log(`Command <${command}> was not recognized`)
